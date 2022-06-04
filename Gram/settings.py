0bef0ea13cff5cb49b238eb57e201ffa78cc7ea0
atienoobwanda <atienoobwanda@gmail.com>
2022-06-04 03:23:39 +0300

    setting up application urls and updating the installed app

--- Gram/settings.py
+++ Gram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'app',
     'users',
 
@@ -121,10 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGIN_URL = 'login'
+# LOGIN_REDIRECT_URL = 'home'
+# LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
