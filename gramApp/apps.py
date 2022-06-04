a80b8b40fefc921af795e7021ebc868474d49c0b
atienoobwanda <atienoobwanda@gmail.com>
2022-06-04 03:43:11 +0300

    updatig the application settings and urls, ready to start development

--- gramApp/apps.py
+++ gramApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class GramappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    name = 'gramApp'
