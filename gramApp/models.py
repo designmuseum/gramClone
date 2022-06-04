1e32b88f3793e1150da75f3580b1ed439dbcc8d6
atienoobwanda <atienoobwanda@gmail.com>
2022-06-04 10:46:40 +0300

    updating image model to return caption

--- gramApp/models.py
+++ gramApp/models.py
@@ -10,7 +10,7 @@ class Image(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.imageName
+        return self.caption
     
     
     @classmethod
