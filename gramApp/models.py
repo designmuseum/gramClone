10bc115099e7bdba2911b0c508756c1755211766
atienoobwanda <atienoobwanda@gmail.com>
2022-06-04 08:34:55 +0300

    adding the images class model

--- gramApp/models.py
+++ gramApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='uploads/')
+    caption = models.TextField()
+    uploadDate = models.DateField(default=timezone.now)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
