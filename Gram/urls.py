e7334c962d4ed4ebd06fec2ff28ab0d1142f7f5e
atienoobwanda <atienoobwanda@gmail.com>
2022-06-04 12:17:47 +0300

    correcting the local variable error on forms

--- Gram/urls.py
+++ Gram/urls.py
@@ -21,10 +21,10 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', include('gramApp.urls')),
     path('register/', user_views.register, name='register'),
     # path('profile/',user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html')),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/login.html')),
+    path ('', include('gramApp.urls')),
 
 ]
--- users/views.py
+++ users/views.py
@@ -5,17 +5,28 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import *
 
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f'Accont created successfully!')
+#             return redirect('login')
+#     else:
+#         form.UserRegistrationForm()
+#     return render(request, 'users/register.html', {'form': form})
+
+    
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Accont created successfully!')
+            messages.success(request, f'Account created successfully! You can now login')
             return redirect('login')
-        else:
-            form.UserRegistrationForm()
-        return render(request, 'users/register.html', {'form': form})
-
-    pass
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
 
