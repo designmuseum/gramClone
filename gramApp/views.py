2d67b19c2e484308b2ed45e86ec40fc798605638
atienoobwanda <atienoobwanda@gmail.com>
2022-06-05 04:37:51 +0300

    creating a view for  new posts

--- gramApp/views.py
+++ gramApp/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 
 
 @login_required
 def feed(request):
     images = Image.getImages()
+    ordering =['-uploadDate'] 
     
-    return render(request, 'app/feed.html', {"images": images})
+    return render(request, 'app/feed.html', {"images": images })
 
 # def home(request):
 #     form = UserRegistrationForm
 #     return render(request, 'app/home.html', {"form": form})
+@login_required
+def addPost(request):
+    
+    return render(request, 'app/home.html')
