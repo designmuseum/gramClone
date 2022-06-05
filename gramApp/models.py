638a02920439bc5626a84c896da8c8eb2eb3b584
atienoobwanda <atienoobwanda@gmail.com>
2022-06-05 19:12:58 +0300

    adding a comment model and making it's migrations

--- gramApp/models.py
+++ gramApp/models.py
@@ -26,3 +26,9 @@ class Image(models.Model):
 
     
 # Profile model is in users app, model.py file
+
+class Comment(models.Model):
+    comment = models.TextField()
+    createDate = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
