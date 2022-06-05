988a8dfb68fd0839e25857d4954283ed5c9ad8b8
atienoobwanda <atienoobwanda@gmail.com>
2022-06-05 09:18:41 +0300

    updating the view, template and url for new post

--- gramApp/views.py
+++ gramApp/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.utils.decorators import method_decorator
 
 
-@login_required
-def feed(request):
-    images = Image.getImages()
+# @login_required
+# def feed(request):
+#     images = Image.getImages()
     
-    return render(request, 'app/feed.html', {"images": images })
+#     return render(request, 'app/feed.html', {"images": images })
 
+
+@method_decorator(login_required, name='dispatch')
 class ImageListView(ListView):
     model = Image
     template_name = 'app/feed.html'
@@ -17,12 +20,27 @@ class ImageListView(ListView):
     ordering =['-uploadDate'] 
 
 
+
+@method_decorator(login_required, name='dispatch')
 class ImageDetailView(DetailView):
     model = Image
     template_name = 'app/ImgDetail.html'    
     context_object_name = 'image'
 
-@login_required
-def addPost(request):
+@method_decorator(login_required, name='dispatch')
+class ImageCreateView(CreateView):
+    model = Image
+    fields=['image', 'caption']
+    template_name = 'app/newImage.html'    
+    # context_object_name = 'image'
+
+
+
+
+
+
+
+# @login_required
+# def addPost(request):
     
-    return render(request, 'app/home.html')
+#     return render(request, 'app/home.html')
