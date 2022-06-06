4a45434cf4556c274e882a65c08bdf90be621a41
atienoobwanda <atienoobwanda@gmail.com>
2022-06-06 04:44:34 +0300

    styled input form

--- gramApp/forms.py
+++ gramApp/forms.py
@@ -4,23 +4,13 @@ from django import forms
 from .models import Image, imgComment
 
 
-
-
-# class newPostForm(forms.ModelForm):
-#     image = forms.ImageField(required=True)
-#     caption = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Caption'}), required=True)
-#     class Meta:
-#         model = Image
-#         fields=['image', 'caption']
-
-
 class commentForm(forms.ModelForm):
     comment = forms.CharField(
-        label='',
-        widget=forms.Textarea(attrs={
-            'rows':'3',
-            'placeholder': 'Comment'
+        label='' ,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Add a comment'
         })
+       
     )
     class Meta:
         model = imgComment
