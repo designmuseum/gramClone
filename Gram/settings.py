7fb93e22d118437bf042edb07e44febb2f12bdd8
atienoobwanda <atienoobwanda@gmail.com>
2022-06-07 21:36:40 +0300

    moving from sqllite to postgres

--- Gram/settings.py
+++ Gram/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'Gram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gram',
+        'USER': 'atieno',
+        'PASSWORD':'mishi',
+        'HOST':'localhost',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
